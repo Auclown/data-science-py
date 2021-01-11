386a7afb4702ba913367761e29ce6f5e28293f3f
Auclown <auclown@gmail.com>
2021-01-11 23:11:28 +1300

    11th/Nov/2021 - access to columns, adding a new column

--- pandas/02_dataframes.py
+++ pandas/02_dataframes.py
@@ -10,3 +10,19 @@ df = pd.DataFrame(data=randn(5, 4), index=[
                   'A', 'B', 'C', 'D', 'E'], columns=['W', 'X', 'Y', 'Z'])
 
 print(df)
+print("")
+
+# DataFrame is a bunch of series
+
+# Can access to data with column name, for example:
+print(df['W'])
+print("")
+
+# Multiple column names is also available:
+print(df[['W', 'Z']])
+print("")
+
+# Create a new column named 'new' by adding two existing columns:
+df['new'] = df['W'] + df['Y']
+print(df['new'])
+print("")
