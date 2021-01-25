2a6f5a6a697b70c451552122683e112ff949337f
Auclown <auclown@gmail.com>
2021-01-25 23:06:52 +1300

    25th/Jan/2021

--- pandas/09_input-output.py
+++ pandas/09_input-output.py
@@ -1,7 +1,9 @@
 # Can use pandas to read CSV, SQL, HTML and Excel files.
+from sqlalchemy import create_engine
 import pandas as pd
 
-### CSV Files ####
+
+############### CSV Files ################
 # read_csv() reads a csv file
 print(pd.read_csv('example.csv'))
 print("")
@@ -15,7 +17,8 @@ print("")
 df.to_csv('my_output.csv', index=False)
 # This will create "my_output.csv" file in current directory
 
-#### Excel Files ####
+
+################ Excel Files ############
 # read_excel() reads an excel file
 # Can use sheetname parameter to read a specific sheet from the excel file
 print(pd.read_excel('Excel_Sample.xlsx', sheet_name='Sheet1'))
@@ -24,3 +27,11 @@ print("")
 # to_excel() creates an excel file out of a dataframe
 # Can also specify the sheet name if want
 df.to_excel('Excel_Sample2.xlsx', sheet_name='NewSheet')
+
+
+################ HTML input ################
+# Can read html
+data = pd.read_html(
+    'https://www.fdic.gov/bank/individual/failed/banklist.html')
+print(data[0].head)
+print("")
